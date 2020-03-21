5b6fe108f2b30399ebb7281e3f616272db3d6d63
Philipp Oldenburg <philipp@oldenburgs.eu>
2020-03-21 16:04:06 +0100

    Switch to a German map

--- frontend/application.py
+++ frontend/application.py
@@ -5,24 +5,22 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
+from germany import create_germany
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.scripts.config.serve_locally=True
 
 
-us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
-fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
-                        color_discrete_sequence=["red"], zoom=3, height=600, size=[100]*1000)
-
-
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+def create_figure():
+    germany = create_germany()
+    return germany
 
 
 app.layout = html.Div(children=[
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=create_figure()
     )
 ])
 
