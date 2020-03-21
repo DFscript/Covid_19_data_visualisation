0328dc541367b5b91d60b9e6478420c632ffcf7f
Mirko <mirko.kunze@web.de>
2020-03-21 22:26:27 +0100

    use better county points

---
+++ frontend/county_centers/filter_landkreise.py
@@ -0,0 +1,21 @@
+import json
+import numpy as np
+
+# https://public.opendatasoft.com/explore/dataset/landkreise-in-germany/export/
+
+with open('landkreise-in-germany.json', 'r') as fid:
+	data = json.load(fid)
+
+result = {}
+
+for item in data:
+	try:
+		id = item["fields"]["cca_2"]
+		p = item["fields"]["geo_point_2d"]
+
+		result[id] = p
+	except:
+		print(item["fields"]["name_2"], "has no id")
+
+with open('landkreise_marker.json', 'w') as fid:
+	json.dump(result, fid, ensure_ascii=False)
