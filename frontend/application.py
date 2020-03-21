4683c0ac4277a2a8c8a3afddf4c143319069ac78
ufpne <max.schemmer@gmx.de>
2020-03-21 12:59:24 +0100

    added stylesheet

--- frontend/application.py
+++ frontend/application.py
@@ -6,24 +6,30 @@ import pandas as pd
 import plotly.graph_objs as go
 
  
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['assetes/external_sytlesheet.css']
  
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
  
 app.scripts.config.serve_locally=True
 
 def clean_data(df):
-    df =  df[df['timestamp'].notna()]
+    '''
+    Clean the data e.g. drop na
+    '''
+    df =  df[df['timestamp'].notna()] #drop na in column timestamp
     return df
 
 def read_data():
+    '''
+    load the data
+    '''
     df_cases = clean_data(pd.read_csv('data-cases/RKI_Corona_Bundesländer.csv'))
     df_actions = clean_data(pd.read_csv('data-actions/policymeasures - measures_taken.csv'))
     return df_actions
 
 def build_bar_chart_data():
     df_actions = read_data()
-    x = df_actions.timestamp
+    x = df_actions["timestamp"]
     y = [3] * len(x)
     data = go.Bar(
         x=x,
@@ -38,6 +44,12 @@ def create_bar_chart():
     bar_fig = go.Figure(data=bar_data, layout=bar_layout)
     bar_chart = dcc.Graph(id="", figure=bar_fig)
     return bar_chart
+
+def normalize_data():
+    '''
+    normalize the data with population
+    :return:
+    '''
 
 bar_chart = create_bar_chart()
 
