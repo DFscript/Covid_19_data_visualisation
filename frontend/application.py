7a8979055a730a84d2d2b24c8791b43da98c276b
Mirko <mirko.kunze@web.de>
2020-03-21 15:36:49 +0100

    basic map example

--- frontend/application.py
+++ frontend/application.py
@@ -2,34 +2,29 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
- 
+import plotly.express as px
+import pandas as pd
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
- 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
- 
 app.scripts.config.serve_locally=True
- 
- 
+
+
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["red"], zoom=3, height=600, size=[100]*1000)
+
+
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+
 app.layout = html.Div(children=[
-    html.H1(children='Hallo, und bleib gesund!'),
- 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
- 
     dcc.Graph(
         id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization hehe2'
-            }
-        }
+        figure=fig
     )
 ])
- 
+
 if __name__ == '__main__':
     app.run_server(host="0.0.0.0", debug=True)
