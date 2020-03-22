f3efcf236fee5bbcbcd5b7b1f7ac6890fbaff8e1
Philipp Oldenburg <philipp@oldenburgs.eu>
2020-03-22 16:51:46 +0100

    Fix bar selection bug when counties selected

--- frontend/application.py
+++ frontend/application.py
@@ -337,7 +337,7 @@ def create_figure(bubble_for_each_county):
         fig = px.scatter_mapbox(df, lat='lat', lon='lon', size="infected", mapbox_style='open-street-map',
                                 animation_frame='timestamp', height=800, hover_data=['country', 'county', 'infected',
                                                                                      'deaths'],
-                                custom_data=['county', 'country'])
+                                custom_data=['country', 'county'])
     # fig = px.scatter_geo(df, hover_name="county", size="infected", animation_frame="timestamp",
     #                      projection="natural earth")
 
