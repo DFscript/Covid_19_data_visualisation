31c9ad159d3790f86444f7b11ef706d65ac60256
Philipp Oldenburg <philipp@oldenburgs.eu>
2020-03-22 13:41:33 +0100

    Make map save zoom when toggling

--- frontend/application.py
+++ frontend/application.py
@@ -3,6 +3,7 @@ import itertools
 import json
 
 import dash
+import plotly.graph_objects as go
 import dash_core_components as dcc
 import dash_daq as daq
 import dash_html_components as html
@@ -113,21 +114,69 @@ def create_figure(bubble_for_each_county):
     #                      projection="natural earth")
 
     fig.layout['clickmode'] = 'event+select'
+    fig.layout['uirevision'] = 'x'
 
     return fig
 
 
+def create_bar():
+    years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+             2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012]
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=years,
+                         y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                            350, 430, 474, 526, 488, 537, 500, 439],
+                         name='Rest of world',
+                         marker_color='rgb(55, 83, 109)'
+                         ))
+    fig.add_trace(go.Bar(x=years,
+                         y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                            299, 340, 403, 549, 499],
+                         name='China',
+                         marker_color='rgb(26, 118, 255)'
+                         ))
+
+    fig.update_layout(
+        title='US Export of Plastic Scrap',
+        xaxis_tickfont_size=14,
+        yaxis=dict(
+            title='USD (millions)',
+            titlefont_size=16,
+            tickfont_size=14,
+        ),
+        legend=dict(
+            x=0,
+            y=1.0,
+            bgcolor='rgba(255, 255, 255, 0)',
+            bordercolor='rgba(255, 255, 255, 0)'
+        ),
+        barmode='group',
+        bargap=0.15,  # gap between bars of adjacent location coordinates.
+        bargroupgap=0.1  # gap between bars of the same location coordinate.
+    )
+    return fig
+
+
 app.layout = html.Div(children=[
-    daq.ToggleSwitch(
-        id='county-country-switch',
-        label='County/Country',
-        labelPosition='bottom'
-    ),
-    dcc.Graph(
-        id='map',
-        figure=create_figure(False)
-    ),
-    html.Div(id='click-data')
+    html.Div(children=[
+        daq.ToggleSwitch(
+            id='county-country-switch',
+            label='Landkreis/Bundesland',
+            labelPosition='bottom'
+        ),
+        dcc.Graph(
+            id='map',
+            figure=create_figure(False)
+        ),
+        html.Div(id='click-data')
+    ], style={'width': '50%', 'float': 'left'}),
+    html.Div(children=[
+        dcc.Graph(
+            id='bar',
+            figure=create_bar()
+        )
+    ], style={'width': '50%', 'float': 'right'}),
 ])
 
 
